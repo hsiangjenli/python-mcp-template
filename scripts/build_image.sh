59befe924a16fccac951e5beb31545805128c83b
Hsiang-Jen Li <hsiangjenli@gmail.com>
2025-07-13 13:48:15 +0800

    feat: update build script to use --no-cache option for Docker build

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,5 +2,5 @@
 
 date_tag=$(date +%Y-%m-%d)
 
-docker build -t python-mcp-template:$date_tag .
+docker build --no-cache -t python-mcp-template:$date_tag .
 docker tag python-mcp-template:$date_tag python-mcp-template:latest
